4e96fe9e19dc2a132ae57f26cccc96a4d3491496
Kunal khuteta <khutetakunal@gmail.com>
2025-09-20 07:48:36 +0530

    chatbot added .env updated 1

--- server/index.js
+++ server/index.js
@@ -13,14 +13,24 @@ const app = express();
 
 
 const allowedOrigins = [
-  "http://localhost:3000",                // when you test locally
-  "https://smart-quiz-system-uuqh.vercel.app/"      // your deployed frontend on vercel
+  "http://localhost:3000", 
+  "https://smart-quiz-system-uuqh.vercel.app", // 👈 your frontend
 ];
 
 app.use(cors({
-  origin: allowedOrigins,
+  origin: function(origin, callback) {
+    // allow requests with no origin (like Postman)
+    if (!origin) return callback(null, true);
+    if (allowedOrigins.indexOf(origin) === -1) {
+      const msg = `The CORS policy for this site does not allow access from the specified Origin: ${origin}`;
+      return callback(new Error(msg), false);
+    }
+    return callback(null, true);
+  },
   credentials: true
 }));
+
+// app.options("*", cors());
   
 app.use(express.json());
 
